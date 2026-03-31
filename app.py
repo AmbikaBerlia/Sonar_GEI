cea0c3a085707a641d5aa25f1162a01b1c31e98e
AmbikaBerlia <150990716+AmbikaBerlia@users.noreply.github.com>
2026-03-31 22:03:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-def greet():
-    print("Hello Sonar")
+def add(a, b):
+    return a + b
 
-greet()
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print(add(5, 3))
+    print(subtract(10, 4))
